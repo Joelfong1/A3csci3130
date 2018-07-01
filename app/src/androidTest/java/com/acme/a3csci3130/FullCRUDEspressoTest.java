dd9c1916c401c0a202dd0f3c711bf9ef4533c5d8
Joelfong1 <Joelfong1@gmail.com>
2018-06-30 22:56:20 -0300

    Update Espresso tests
    All tests passing. Wasn't sure how to check listview fields so I just delete everything every test run.

--- app/src/androidTest/java/com/acme/a3csci3130/FullCRUDEspressoTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/FullCRUDEspressoTest.java
@@ -6,12 +6,17 @@ package com.acme.a3csci3130;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -33,6 +38,14 @@ public class FullCRUDEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+    //Clear out everything so tests can run
+    private DatabaseReference Firebase;
+    @Before
+    public void setUp() throws Exception {
+        Firebase = FirebaseDatabase.getInstance().getReference("businesses");
+        Firebase.removeValue();
+    }
+
     @Test
     public void testCreation() {
         onView(withId(R.id.submitButton)).perform(click());
@@ -50,12 +63,29 @@ public class FullCRUDEspressoTest {
         onView(withId(R.id.provinceSpinner)).check(matches(withSpinnerText(containsString("NS"))));
 
         onView(withId(R.id.submitButton)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).check(matches(isDisplayed()));
     }
 
     @Test
     public void testRead() {
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).perform(click());
+        onView(withId(R.id.submitButton)).perform(click());
+
+        //Go through creation steps
+        onView(withId(R.id.name)).perform(typeText("Test Business"));
+        onView(withId(R.id.businessNumber)).perform(typeText("928394764"));
+        onView(withId(R.id.address)).perform(typeText("123 Peggy's Cove Road"), closeSoftKeyboard());
+
+        onView(withId(R.id.businessSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Fisher"))).perform(click());
+        onView(withId(R.id.businessSpinner)).check(matches(withSpinnerText(containsString("Fisher"))));
+        onView(withId(R.id.provinceSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("NS"))).perform(click());
+        onView(withId(R.id.provinceSpinner)).check(matches(withSpinnerText(containsString("NS"))));
+
+        onView(withId(R.id.submitButton)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).check(matches(isDisplayed()));
+
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
 
         onView(withId(R.id.name)).check(matches(withText("Test Business")));
         onView(withId(R.id.businessNumber)).check(matches(withText("928394764")));
@@ -67,13 +97,29 @@ public class FullCRUDEspressoTest {
 
     @Test
     public void testUpdate() throws Exception{
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).perform(click());
+        onView(withId(R.id.submitButton)).perform(click());
+
+        //Go through creation steps
+        onView(withId(R.id.name)).perform(typeText("Test Business"));
+        onView(withId(R.id.businessNumber)).perform(typeText("928394764"));
+        onView(withId(R.id.address)).perform(typeText("123 Peggy's Cove Road"), closeSoftKeyboard());
+
+        onView(withId(R.id.businessSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Fisher"))).perform(click());
+        onView(withId(R.id.businessSpinner)).check(matches(withSpinnerText(containsString("Fisher"))));
+        onView(withId(R.id.provinceSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("NS"))).perform(click());
+        onView(withId(R.id.provinceSpinner)).check(matches(withSpinnerText(containsString("NS"))));
+
+        onView(withId(R.id.submitButton)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
 
         Thread.sleep(1000);
 
-        onView(withId(R.id.name)).perform(typeText("Test Business2"));
-        onView(withId(R.id.businessNumber)).perform(typeText("111111111"));
-        onView(withId(R.id.address)).perform(typeText("1233 LeMarchant Street"), closeSoftKeyboard());
+        onView(withId(R.id.name)).perform(replaceText("Test Business2"));
+        onView(withId(R.id.businessNumber)).perform(replaceText("111111111"));
+        onView(withId(R.id.address)).perform(replaceText("1233 LeMarchant Street"), closeSoftKeyboard());
 
         onView(withId(R.id.businessSpinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("Distributor"))).perform(click());
@@ -82,8 +128,8 @@ public class FullCRUDEspressoTest {
         onData(allOf(is(instanceOf(String.class)), is("AB"))).perform(click());
         onView(withId(R.id.provinceSpinner)).check(matches(withSpinnerText(containsString("AB"))));
 
-        onView(withId(R.id.submitButton)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).check(matches(isDisplayed()));
+        onView(withId(R.id.updateButton)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
 
         onView(withId(R.id.name)).check(matches(withText("Test Business2")));
         onView(withId(R.id.businessNumber)).check(matches(withText("111111111")));
@@ -95,12 +141,25 @@ public class FullCRUDEspressoTest {
 
     @Test
     public void testDelete() throws Exception{
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).perform(click());
+        onView(withId(R.id.submitButton)).perform(click());
+
+        //Go through creation steps
+        onView(withId(R.id.name)).perform(typeText("Test Business"));
+        onView(withId(R.id.businessNumber)).perform(typeText("928394764"));
+        onView(withId(R.id.address)).perform(typeText("123 Peggy's Cove Road"), closeSoftKeyboard());
+
+        onView(withId(R.id.businessSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Fisher"))).perform(click());
+        onView(withId(R.id.businessSpinner)).check(matches(withSpinnerText(containsString("Fisher"))));
+        onView(withId(R.id.provinceSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("NS"))).perform(click());
+        onView(withId(R.id.provinceSpinner)).check(matches(withSpinnerText(containsString("NS"))));
+
+        onView(withId(R.id.submitButton)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
 
         Thread.sleep(1000);
-
         onView(withId(R.id.deleteButton)).perform(click());
-
-        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(6).check(matches(not(isDisplayed())));
-    }
+        onView(withId(R.id.listView)).check(matches(not(isDisplayed())));    }
 }
